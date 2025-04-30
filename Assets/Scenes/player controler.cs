094dc31b6d0827c149e9f0a8b42f04c31e20c115
bita-he <bita.heydari0625@gmail.com>
2025-04-30 20:36:35 +0430

    chahnge project please check it

---
+++ Assets/Scenes/player controler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    float speed = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        speed = 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+      public void Movement()
+      {
+        var xAxis = Input.GetAxisRaw("Horizontal");
+        Debug.Log(xAxis);
+        var force = new Vector2(xAxis * (speed * Time.deltaTime), 0);
+        _rb.AddForce(force, ForceMode2D.Impulse);
+      }
+}
